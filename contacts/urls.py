2f1e1363565b845daea929933bc830ed073ad8c1
noamolat-fs <noamolat@fullscale.io>
2023-06-30 19:46:14 +0800

    Add CRUD + Search functionalities

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "contacts"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:uid>/", views.read, name="read"),
+    path("<int:uid>/update/", views.update, name="update"),
+    path("<int:uid>/", views.read, name="delete"),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,61 @@
 import typing
-from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.urls import reverse
+
+from contacts.models import Contact
 
 # Create your views here.
 def index(request: HttpRequest):
-    return HttpResponse("Hello, world. You're at the contacts index.")
+    query = request.GET.get("query", "")
+    contacts = Contact.objects.filter(name__icontains=query) if query else Contact.objects.all()
+    context = dict(
+        query=query,
+        contacts=contacts,
+    )
+    return render(request, "contacts/index.html", context)
+
+def create(request: HttpRequest):
+    if request.method == "GET":
+        return render(request, "contacts/create.html")
+    elif request.method == "POST":
+        post = request.POST
+        kwargs = dict(
+            name=post["name"],
+            address=post["address"],
+            email=post["email"],
+            mobile=post["mobile"],
+        )
+        Contact(**kwargs).save()
+        return HttpResponseRedirect(reverse("contacts:index"))
+
+def read(request: HttpRequest, uid: int):
+    contact = get_object_or_404(Contact, pk=uid)
+    if request.method == "GET": return view(request, contact)
+    elif request.method == "POST": return delete(request, contact)
+
+def view(request: HttpRequest, contact: Contact):
+    context = dict(
+        contact=contact,
+    )
+    return render(request, "contacts/read.html", context)
+
+def update(request: HttpRequest, uid: int):
+    contact = get_object_or_404(Contact, pk=uid)
+    if request.method == "GET":
+        return render(request, "contacts/update.html", dict(contact=contact))
+    elif request.method == "POST":
+        post = request.POST
+        kwargs = dict(
+            name=post["name"],
+            address=post["address"],
+            email=post["email"],
+            mobile=post["mobile"],
+        )
+        Contact(id=contact.id, **kwargs).save()
+        return HttpResponseRedirect(reverse("contacts:index"))
+
+def delete(request: HttpRequest, contact: Contact):
+    contact.delete()
+    return HttpResponseRedirect(reverse("contacts:index"))
